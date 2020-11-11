6bf0f6406276dc8613a5275d4d2af32d3acad442
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-11-12 00:22:33 +0530

    Changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Welcome({ name }) {
   return (
     <>
-      <h1>Hey!{name}</h1>
+      Hey!<h1>{name}</h1>
       <h2>Welcome to Newton School.</h2>
     </>
   );
